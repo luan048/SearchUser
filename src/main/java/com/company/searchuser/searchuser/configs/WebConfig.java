28f6151624402aacabd93686b9e4a2020e24a667
luan048 <luanmonteiropedrosa@gmail.com>
2024-12-12 21:59:16 -0300

    Add commit

--- src/main/java/com/company/searchuser/searchuser/configs/WebConfig.java
+++ src/main/java/com/company/searchuser/searchuser/configs/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173") // URL do front-end
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
