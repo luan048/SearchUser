1ec53d359cfa018deba2f7eac74a0a0bc9e381ec
luan048 <luanmonteiropedrosa@gmail.com>
2024-12-12 21:07:03 -0300

    Update in the Cors

--- src/main/java/com/company/searchuser/searchuser/configs/WebConfig.java
+++ src/main/java/com/company/searchuser/searchuser/configs/WebConfig.java
@@ -2,13 +2,16 @@ package com.company.searchuser.searchuser.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**");
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
